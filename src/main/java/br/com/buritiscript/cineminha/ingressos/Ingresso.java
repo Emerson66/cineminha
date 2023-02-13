f7a5eeb8dc01519fa64e1ee9fc3c1ba0ad574ad7
Emerson66 <emersonreisdacosta@gmail.com>
2023-02-13 16:06:43 -0400

    Criado a entidade Ingresso

--- src/main/java/br/com/buritiscript/cineminha/ingressos/Ingresso.java
+++ src/main/java/br/com/buritiscript/cineminha/ingressos/Ingresso.java
@@ -1,6 +1,22 @@
 package br.com.buritiscript.cineminha.ingressos;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
 
+@Entity
+@Data
 public class Ingresso {
     
+    private Long id;
+
+    private Tipo tipo;
+
+    private String documento;
+
+    private Sessao sessao;
+
+    @Deprecated
+    public Ingresso (){
+
+    }
 }
