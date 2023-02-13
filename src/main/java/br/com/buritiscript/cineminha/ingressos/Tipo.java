38b7dd51b1381d7d881f3bccdfa8857ea753f8c9
Emerson66 <emersonreisdacosta@gmail.com>
2023-02-13 16:12:37 -0400

    Criado Enum Tipo

---
+++ src/main/java/br/com/buritiscript/cineminha/ingressos/Tipo.java
@@ -0,0 +1,23 @@
+package br.com.buritiscript.cineminha.ingressos;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public enum Tipo {
+    INTEIRA {
+        @Override
+        public BigDecimal calcula(BigDecimal preco){
+            return preco;
+        }
+    },
+    MEIA_ENTRADA {
+        @Override
+        public BigDecimal calcula(BigDecimal preco) {
+            return preco.divide(new BigDecimal("2.0"), RoundingMode.CEILING);
+        }
+    };
+
+    public abstract BigDecimal calcula(BigDecimal preco);
+
+}
+
